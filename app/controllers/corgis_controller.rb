82358a964edf8ee39dfa6532aef5afc51652bf1b
Samantha Aleman <samantha.aleman25@gmail.com>
2020-11-20 12:57:15 -0800

    fixed an error in my corgi controller

--- app/controllers/corgis_controller.rb
+++ app/controllers/corgis_controller.rb
@@ -1,22 +1,17 @@
 class CorgisController < ApplicationController
 
-def index
-    #render :json => Corgi.all
-    @corgis = Corgi.all
-    render json: CorgiSerializer.new(@corgi).to_serialized_json
-end 
 
 def update
     set_corgi
     @corgi.update(corgi_params)
-    render json: CorgiSerializer.new(corgi).to_serialized_json
+    render @corgi
 end 
 
 def create
     @corgi = Corgi.new(corgi_params)
     corgi.user = current_user
     if @corgi.save 
-        render json: CorgiSerializer.new(corgi).to_serialized_json
+        render @corgi
     else
         render json: {errors: "oops something went wrong :3"}, status: :not_acceptable
     end 
